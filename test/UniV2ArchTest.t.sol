96a5e4e01399395dbb0229aa04b9739706fa7de5
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2026-03-03 18:17:41 +0100

    testPairReserves

--- test/UniV2ArchTest.t.sol
+++ test/UniV2ArchTest.t.sol
@@ -1,28 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-
 import "forge-std/Test.sol";
-
 
 // INTERFACES
 
 // ERC20 Interface
 interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
 
- function  balanceOf(address account) external view  returns (uint256);
-
- function approve(address spender, uint256 amount) external returns(bool);
-     
-} 
-
+    function approve(address spender, uint256 amount) external returns (bool);
+}
 
 // Uniswap V2 Factory Interface
 interface IUniswapV2Factory {
-
-    function getPair(address tokenA, address tokenB) external view returns(address pair);
+    function getPair(
+        address tokenA,
+        address tokenB
+    ) external view returns (address pair);
 }
 
+// Uniswap V2 Pair Interface
+interface IUniswapV2Pair{
+    function token0() external view returns (address);
+    function token1() external view returns(address);
+    function getReserves( )external view returns(uint112 reserve0,uint112 reserve1, uint32 blockTimestampLast);
+}
 
 // Uniswap V2 Router Interface
 interface IUniswapV2Router02 {
@@ -36,45 +39,61 @@ interface IUniswapV2Router02 {
     ) external payable returns (uint[] memory amounts);
 }
 
-
-
-
 contract UniV2ArchTest is Test {
+    address constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address WETH;
 
-address constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
-address constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-address WETH;
+    IUniswapV2Factory factory = IUniswapV2Factory(FACTORY);
+    IUniswapV2Router02 router = IUniswapV2Router02(ROUTER);
+    IERC20 usdc = IERC20(USDC);
+
+    // setup function to create the fork and set the WETH address(ROUTER)
+    function setUp() public {
+        string memory rpcUrl = vm.envString("MAINNET_RPC_URL");
+
+        uint256 forkBlock = vm.envUint("FORK_BLOCK");
+
+        // vm.createSelectFork(rpcUrl, forkBlock);
+        vm.createSelectFork(rpcUrl, forkBlock);
+        vm.deal(address(this), 100 ether);
+        WETH = router.WETH();
+    }
+
+    // first test to ensure the fork is working and we can access the WETH address (FACTORY)
+    function test_fork_runs() public {
+        emit log_named_uint("Fork Block Number", block.number);
+        emit log_named_address("WETH", WETH);
+        assertTrue(block.number > 0);
+    }
 
 
-IUniswapV2Factory factory = IUniswapV2Factory(FACTORY);
-IUniswapV2Router02 router = IUniswapV2Router02(ROUTER);
-IERC20 usdc = IERC20(USDC);
+    // test to ensure the factory can find the WETH/USDC pair
+    function test_factory_finds_pair() public {
+        address pair = factory.getPair(WETH, USDC);
+        emit log_named_address("WETH/USDC Pair", pair);
+        assertTrue(pair != address(0));
+    }
 
 
-function setUp() public {
 
-    string memory rpcUrl = vm.envString("MAINNET_RPC_URL");
+    // test to check the reserves of the WETH/USDC pair
+    function test_pair_reserves() public {
+        address pairAddress = factory.getPair(WETH, USDC);
+        IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
 
-    uint256 forkBlock = vm.envUint("FORK_BLOCK");
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
 
-    // vm.createSelectFork(rpcUrl, forkBlock);
-    vm.createSelectFork(rpcUrl, forkBlock);
-    vm.deal(address(this), 100 ether);
-    WETH = router.WETH();
-}
+        emit log_named_address("token0", pair.token0());
+        emit log_named_address("token1", pair.token1());
+        emit log_named_uint("reserve0", reserve0);
+        emit log_named_uint("reserve1", reserve1);
 
-function test_fork_runs() public {
-    emit log_named_uint("Fork Block Number", block.number);
-    emit log_named_address("WETH", WETH);
-    assertTrue(block.number > 0);
-}
+        assertTrue(reserve0 > 0 && reserve1 > 0);
+    }
 
-function test_factory_finds_pair()public{
-    address pair = factory.getPair(WETH,USDC);
-    emit log_named_address("WETH/USDC Pair", pair);
-    assertTrue(pair != address(0));
-}
+
 
 
 
